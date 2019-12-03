b7ea500ce4140ef12c057df9389e40fc5e81287e
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2019-12-03 12:41:39 +0530

    added applevel tests
    What changes you have made?
    added app tests
    
    Why this change is necessary?
    to ensure app is working properly
    
    What is the impact of the change?
    .....

--- beverage.js
+++ beverage.js
@@ -3,7 +3,6 @@ const { getTransactionDetails } = require("./src/getTransactionDetails.js");
 const { getRecordDataPath, getDate } = require("./src/config");
 
 const main = function() {
-  console.log("Anna Juice Ltd");
   const usrArgs = process.argv.slice(2);
   const path = getRecordDataPath(process.env);
   const date = getDate(process.env);
